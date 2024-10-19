04faead87ca9adcba763e87a274afa48919993bd
andre-A1 <agustinusandre154@gmail.com>
2024-10-19 13:16:31 +0700

    control statement if

---
+++ control steatmen if.txt 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int nilai;
+cout<<"Masukan Sebuah Nilai : ";
+cin>>nilai;
+if(nilai>70)
+cout<<"\n SELAMAT ANDA LULUS";
+else
+cout<<"\nMOHON MAAF ANDA TIDAK LULUS";
+}
